6a5debf656a94ac8de14244a34078671fce6807f
Takeru Sato <type.in.type@gmail.com>
2017-08-01 23:25:33 +0900

    📝 Reformat the project

--- core/src/main/scala/gcp/Storage.scala
+++ core/src/main/scala/gcp/Storage.scala
@@ -9,8 +9,7 @@ import com.google.cloud.storage.{BlobId, Storage => GStorage, StorageOptions}
 
 trait Storage {
   def fetch[F[_]](bucket: String, name: String, chunkSize: Int, options: GStorage.BlobSourceOption*)(
-      implicit F: MonadError[F, Throwable]
-  ): F[InputStream]
+      implicit F: MonadError[F, Throwable]): F[InputStream]
 }
 
 object Storage {
@@ -18,38 +17,50 @@ object Storage {
   def apply()(implicit storageService: GStorage): Storage = new Storage {
 
     def fetch[F[_]](bucket: String, name: String, chunkSize: Int, options: GStorage.BlobSourceOption*)(
-        implicit F: MonadError[F, Throwable]
-    ): F[InputStream] =
+        implicit F: MonadError[F, Throwable]): F[InputStream] =
       F.catchNonFatal(transform(storageService.reader(BlobId.of(bucket, name), options: _*), chunkSize))
   }
 
-  def transform(ch: ReadChannel, chunkSize: Int): InputStream = new InputStream {
+  def transform(ch: ReadChannel, chunkSize: Int): InputStream =
+    new InputStream {
 
-    private[this] val buffer: ByteBuffer =
-      ByteBuffer.allocateDirect(chunkSize)
+      private[this] val buffer: ByteBuffer =
+        ByteBuffer.allocateDirect(chunkSize)
 
-    private[this] lazy val initial: Int = {
-      val x = ch.read(buffer)
-      buffer.flip()
-      x
+      private[this] lazy val initial: Int = {
+        val x = ch.read(buffer)
+        buffer.flip()
+        x
+      }
+
+      private def more: Int = {
+        buffer.clear()
+        ch.read(buffer)
+      }
+
+      private def first: Int = {
+        buffer.flip()
+        buffer.get & 0xff
+      }
+
+      override def available(): Int =
+        buffer.remaining()
+
+      override def read(): Int =
+        if (initial <= 0) -1
+        else if (buffer.hasRemaining) buffer.get & 0xff
+        else if (more <= 0) -1
+        else first
+
+      override def close(): Unit = {
+        super.close()
+        ch.close()
+        buffer.clear()
+      }
     }
-
-    override def available(): Int = buffer.remaining()
-
-    override def read(): Int =
-      if (initial <= 0) -1
-      else if (buffer.hasRemaining) buffer.get & 0xff
-      else if ({ buffer.clear(); ch.read(buffer) } <= 0) -1
-      else { buffer.flip(); buffer.get & 0xff }
-
-    override def close(): Unit = {
-      super.close()
-      ch.close()
-      buffer.clear()
-    }
-  }
 
   object Service {
-    implicit lazy val default: GStorage = StorageOptions.getDefaultInstance.getService
+    implicit lazy val default: GStorage =
+      StorageOptions.getDefaultInstance.getService
   }
 }
